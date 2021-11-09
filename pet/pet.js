d508a9bf69cbb7082d042cf64688743558d88517
ratzhe <81121205+ratzhe@users.noreply.github.com>
2021-11-09 11:28:52 -0300

    add

---
+++ pet/pet.js
@@ -0,0 +1,216 @@
+//CÓDIGO JS DAS ROUPAS DE PET
+function roupa1Frente() {
+  document.getElementById("roupa1").src="https://img.ltwebstatic.com/images3_pi/2021/05/12/1620784178919d43cb11ab6d84b0ba897ec31276fe_thumbnail_600x.webp";
+}
+
+function roupa1Costas() {
+  document.getElementById("roupa1").src="https://img.ltwebstatic.com/images3_pi/2021/05/12/162078418438acf965c50fe243f2fd51fe716db5a1_thumbnail_600x.webp";
+}
+
+function roupa2Frente() {
+  document.getElementById("roupa2").src="https://img.ltwebstatic.com/images3_pi/2021/05/07/16203540936e7a31ed087ac961951f0b6fb50d3d64_thumbnail_600x.webp";
+}
+
+function roupa2Costas() {
+  document.getElementById("roupa2").src="https://img.ltwebstatic.com/images3_pi/2021/05/07/16203540858273351e354ce62d055051c09e421d4f_thumbnail_600x.webp";
+}
+
+function roupa3Frente() {
+  document.getElementById("roupa3").src="https://img.ltwebstatic.com/images3_pi/2021/05/07/16203541164b597d4d672807b229635d8c01cd9be7_thumbnail_600x.webp";
+}
+
+function roupa3Costas() {
+  document.getElementById("roupa3").src="https://img.ltwebstatic.com/images3_pi/2021/05/07/1620354096c90a63ad8f56ad0a2fd2f91dcf98a1e2_thumbnail_600x.webp";
+}
+
+function roupa4Frente() {
+  document.getElementById("roupa4").src="https://img.ltwebstatic.com/images3_pi/2021/05/28/1622169211bcca3d8a13c783b287424838902ae159_thumbnail_600x.webp";
+}
+
+function roupa4Costas() {
+  document.getElementById("roupa4").src="https://img.ltwebstatic.com/images3_pi/2021/05/28/1622169199bdda6b762fb8458eefc36f6a6d79714f_thumbnail_600x.webp";
+}
+ 
+function roupa5Frente() {
+  document.getElementById("roupa5").src="https://img.ltwebstatic.com/images3_pi/2021/07/06/16255598329fc83a1018c9a61e7aa862156b49da60_thumbnail_600x.webp";
+}
+
+function roupa5Costas() {
+  document.getElementById("roupa5").src="https://img.ltwebstatic.com/images3_pi/2021/07/06/1625559843d17bd0768739da54f3727136f010b9ff_thumbnail_600x.webp";
+}
+
+function roupa6Frente() {
+  document.getElementById("roupa6").src="https://img.ltwebstatic.com/images3_pi/2021/09/18/1631955875a90895def08abad164fff396c524b39f_thumbnail_600x.webp";
+}
+
+function roupa6Costas() {
+  document.getElementById("roupa6").src="https://img.ltwebstatic.com/images3_pi/2021/09/18/16319558697a2baca7a2d303bd4f5d299c8f23c1f3_thumbnail_600x.webp";
+}
+
+function roupa7Frente() {
+  document.getElementById("roupa7").src="https://img.ltwebstatic.com/images3_pi/2021/01/05/160981458833093d9d50c5c1e0f4056d1fb562a211_thumbnail_600x.webp";
+}
+ 
+function roupa7Costas() {
+  document.getElementById("roupa7").src="https://img.ltwebstatic.com/images3_pi/2021/01/05/1609814585e6670f9d7529598128095abc48c54ac7_thumbnail_600x.webp";
+}
+
+function roupa8Frente() {
+  document.getElementById("roupa8").src="https://img.ltwebstatic.com/images3_pi/2021/09/06/1630909692f5198e38f1d3892e7faaf15e352d0768_thumbnail_600x.webp";
+}
+
+function roupa8Costas() {
+  document.getElementById("roupa8").src="https://img.ltwebstatic.com/images3_pi/2021/09/06/1630909699c39eea695f339020bb4fb4357f7e6279_thumbnail_600x.webp";
+}
+
+function roupa9Frente() {
+  document.getElementById("roupa9").src="https://img.ltwebstatic.com/images3_pi/2021/08/09/1628476348866d35cdb6f1d2add0c85d8df5b22560_thumbnail_600x.webp";
+}
+ 
+function roupa9Costas() {
+  document.getElementById("roupa9").src="https://img.ltwebstatic.com/images3_pi/2021/08/09/1628476349a4ab8c3c3e5a6f89c12ae151dccbfa41_thumbnail_600x.webp";
+}
+//CÓDIGO JS DOS ACESSÓRIOS DE PETS
+function acessorio1Frente() {
+  document.getElementById("acessorio1").src="https://img.ltwebstatic.com/images3_pi/2020/12/14/1607923303b7ca4e4dcad57f27e4976f924c38968c_thumbnail_600x.webp";
+}
+
+function acessorio1Costas() {
+  document.getElementById("acessorio1").src="https://img.ltwebstatic.com/images3_pi/2020/12/14/1607923310b1857e48beed5c2d894bc7d10c4405b1_thumbnail_600x.webp";
+}
+
+function acessorio2Frente() {
+  document.getElementById("acessorio2").src="https://img.ltwebstatic.com/images3_pi/2021/03/09/1615258712d2fb46627f475827ed284d8804f5a66d_thumbnail_600x.webp";
+}
+ 
+function acessorio2Costas() {
+  document.getElementById("acessorio2").src="https://img.ltwebstatic.com/images3_pi/2021/03/09/16152587171b5065fcecb06f9ce8f71d943683c67c_thumbnail_600x.webp";
+}
+
+function acessorio3Frente() {
+  document.getElementById("acessorio3").src="https://img.ltwebstatic.com/images3_pi/2021/03/02/1614664245fa67b4d1c2a3301186213271048863af_thumbnail_600x.webp";
+}
+ 
+function acessorio3Costas() {
+  document.getElementById("acessorio3").src="https://img.ltwebstatic.com/images3_pi/2021/03/02/1614664252ae99fbb2d10841b0beb79050c692667a_thumbnail_600x.webp";
+}
+
+function acessorio4Frente() {
+  document.getElementById("acessorio4").src="https://img.ltwebstatic.com/images3_pi/2021/06/25/1624586308f61ba1e615320b53ab6322ddafe30064_thumbnail_600x.webp";
+}
+
+function acessorio4Costas() {
+  document.getElementById("acessorio4").src="https://img.ltwebstatic.com/images3_pi/2021/06/25/16245863146ea458f77c804c769648aac6685ccc27_thumbnail_600x.webp";
+}
+
+function acessorio5Frente() {
+  document.getElementById("acessorio5").src="https://img.ltwebstatic.com/images3_pi/2021/09/17/16318570134573c4c2412db69611b059f08f0ae1df_thumbnail_600x.webp";
+}
+
+function acessorio5Costas() {
+  document.getElementById("acessorio5").src="https://img.ltwebstatic.com/images3_pi/2021/09/17/16318570239825458552646973c6803e08d810b4bc_thumbnail_600x.webp";
+}
+
+function acessorio6Frente() {
+  document.getElementById("acessorio6").src="https://img.ltwebstatic.com/images3_pi/2021/07/02/16251940390d07e3e1aec50fc1822832510e323b01_thumbnail_600x.webp";
+}
+
+function acessorio6Costas() {
+  document.getElementById("acessorio6").src="https://img.ltwebstatic.com/images3_pi/2021/07/02/1625194044d954c95402c0323ccecd9f9b355d0f47_thumbnail_600x.webp";
+}
+
+function acessorio7Frente() {
+  document.getElementById("acessorio7").src="https://img.ltwebstatic.com/images3_pi/2020/12/15/1608013690390632e2eafe663f27c4f54b9715758b_thumbnail_600x.webp";
+}
+
+function acessorio7Costas() {
+  document.getElementById("acessorio7").src="https://img.ltwebstatic.com/images3_pi/2020/12/15/16080136977e8366717f1e34fc44696cb32dd3a900_thumbnail_600x.webp";
+}
+
+function acessorio8Frente() {
+  document.getElementById("acessorio8").src="https://img.ltwebstatic.com/images3_pi/2021/04/08/16178499294ec2925ae5d7966f97689d4095329f7d_thumbnail_600x.webp";
+}
+ 
+function acessorio8Costas() {
+  document.getElementById("acessorio8").src="https://img.ltwebstatic.com/images3_pi/2021/04/08/161784993197b60f29cb2a699b55c29e52c14f4009_thumbnail_600x.webp";
+}
+
+function acessorio9Frente() {
+  document.getElementById("acessorio9").src="https://img.ltwebstatic.com/images3_pi/2021/04/08/161784911658ff3830c7482864330105a0ed470277_thumbnail_600x.webp";
+}
+
+function acessorio9Costas() {
+  document.getElementById("acessorio9").src="https://img.ltwebstatic.com/images3_pi/2021/04/08/16178491203ce91d9b3a82cac525de64b7928f9f49_thumbnail_600x.webp";
+}
+
+function brinquedo1Frente() {
+  document.getElementById("brinquedo1").src="https://img.ltwebstatic.com/images3_pi/2021/08/30/16303019960d9646b338882f5b5534eb9172a9a9ec_thumbnail_600x.webp";
+}
+//CÓDIGO JS DO BRINQUEDOS DE PETS
+function brinquedo1Costas() {
+  document.getElementById("brinquedo1").src="https://img.ltwebstatic.com/images3_pi/2021/08/30/163030200687cf19c6abe697081742397dd2d17a58_thumbnail_600x.webp";
+}
+
+function brinquedo2Frente() {
+  document.getElementById("brinquedo2").src="https://img.ltwebstatic.com/images3_pi/2020/09/24/16009149517515b938c22ec4d37183afd2624fe27d_thumbnail_600x.webp";
+}
+
+function brinquedo2Costas() {
+  document.getElementById("brinquedo2").src="https://img.ltwebstatic.com/images3_pi/2020/09/24/160091495370538815e24b086d2c1e96077342083a_thumbnail_600x.webp";
+}
+
+function brinquedo3Frente() {
+  document.getElementById("brinquedo3").src="https://img.ltwebstatic.com/images3_pi/2021/01/11/161034903679b51712cb57bde766de857c4b51ec68_thumbnail_600x.webp";
+}
+
+function brinquedo3Costas() {
+  document.getElementById("brinquedo3").src="https://img.ltwebstatic.com/images3_pi/2021/01/11/16103490397a58566dcfe98f6449384c585344875f_thumbnail_600x.webp";
+}
+
+function brinquedo4Frente() {
+  document.getElementById("brinquedo4").src="https://img.ltwebstatic.com/images3_pi/2020/06/29/1593402054a41e606143031ac902b523589c395a2f_thumbnail_600x.webp";
+}
+
+function brinquedo4Costas() {
+  document.getElementById("brinquedo4").src="https://img.ltwebstatic.com/images3_pi/2020/06/29/1593402057ac5cabbcf9833220aa769c05a68fc041_thumbnail_600x.webp";
+}
+
+function brinquedo5Frente() {
+  document.getElementById("brinquedo5").src="https://img.ltwebstatic.com/images3_pi/2021/09/10/1631243224a44e84245fab45441604dc9778ced6e5_thumbnail_600x.webp";
+}
+ 
+function brinquedo5Costas() {
+  document.getElementById("brinquedo5").src="https://img.ltwebstatic.com/images3_pi/2021/09/10/16312432313968a7685e33aea620457c2c486faba6_thumbnail_600x.webp";
+}
+
+function brinquedo6Frente() {
+  document.getElementById("brinquedo6").src="https://img.ltwebstatic.com/images3_pi/2021/09/15/1631698092d9bd3b27f43167ccfd4c405dd906907d_thumbnail_600x.webp";
+}
+
+function brinquedo6Costas() {
+  document.getElementById("brinquedo6").src="https://img.ltwebstatic.com/images3_pi/2021/09/15/16316981049a7d615c8be25eea691ebe78d196fca0_thumbnail_600x.webp";
+}
+ 
+function brinquedo7Frente() {
+  document.getElementById("brinquedo7").src="https://img.ltwebstatic.com/images3_pi/2021/08/24/162979436389614dbbae05c232d18700bfa0204168_thumbnail_600x.webp";
+}
+
+function brinquedo7Costas() {
+  document.getElementById("brinquedo7").src="https://img.ltwebstatic.com/images3_pi/2021/08/24/1629794367e9f3a61dc9de7a4a08ab532595ab0ae0_thumbnail_600x.webp";
+}
+
+function brinquedo8Frente() {
+  document.getElementById("brinquedo8").src="https://img.ltwebstatic.com/images3_pi/2021/07/05/1625462907dfcc0e51ba75294e8fed032f24f780ec_thumbnail_600x.webp";
+}
+
+function brinquedo8Costas() {
+  document.getElementById("brinquedo8").src="https://img.ltwebstatic.com/images3_pi/2021/07/05/162546291395f981859749e59a9228b9afeaa0db02_thumbnail_600x.webp";
+}
+ 
+function brinquedo9Frente() {
+  document.getElementById("brinquedo9").src="https://img.ltwebstatic.com/images3_pi/2021/08/18/1629277869b5f20a11f58c473efa4dad21359eaf31_thumbnail_600x.webp";
+}
+
+function brinquedo9Costas() {
+  document.getElementById("brinquedo9").src="https://img.ltwebstatic.com/images3_pi/2021/08/18/1629277870ca862753d6f6d3292144780f82c050ff_thumbnail_600x.webp";
+}
