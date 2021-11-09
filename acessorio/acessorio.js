10716ce8ea3d98eef2040caae0cdfd0b773db290
ratzhe <81121205+ratzhe@users.noreply.github.com>
2021-11-09 11:12:16 -0300

    add

---
+++ acessorio/acessorio.js
@@ -0,0 +1,216 @@
+//CÓDIGO JS DOS COLARES
+function colar1Frente() {
+  document.getElementById("colar1").src="https://img.ltwebstatic.com/images3_pi/2021/08/16/16291083720b49e9f83cfa2f38b9dc79f381d8bd55_thumbnail_600x.webp";
+}
+
+function colar1Costas() {
+  document.getElementById("colar1").src="https://img.ltwebstatic.com/images3_pi/2021/08/16/1629108375a8b1509e06c628475454b19a01e12657_thumbnail_600x.webp";
+}
+
+function colar2Frente() {
+  document.getElementById("colar2").src="https://img.ltwebstatic.com/images3_pi/2021/08/15/1629035374aebb2384e2c43eb7b5a731ef3ded89b6_thumbnail_600x.webp";
+}
+
+function colar2Costas() {
+  document.getElementById("colar2").src="https://img.ltwebstatic.com/images3_pi/2021/08/15/16290353825e2d3cab19cad39fba2c80f8a77f0020_thumbnail_600x.webp";
+}
+
+function colar3Frente() {
+  document.getElementById("colar3").src="https://img.ltwebstatic.com/images3_pi/2020/06/18/159244653235f61de2ba2350eeb2da13a118dd9617_thumbnail_600x.webp";
+}
+
+function colar3Costas() {
+  document.getElementById("colar3").src="https://img.ltwebstatic.com/images3_pi/2020/06/18/1592446545d614e9aca56e064357deb5d5a5cf4cfb_thumbnail_600x.webp";
+}
+//CÓDIGO JS DAS PULSEIRAS 
+function pulseira1Frente() {
+  document.getElementById("pulseira1").src="https://img.ltwebstatic.com/images3_pi/2021/07/15/162632767863cb78ccc9e834dc8299e1881a870858_thumbnail_600x.webp";
+}
+
+function pulseira1Costas() {
+  document.getElementById("pulseira1").src="https://img.ltwebstatic.com/images3_pi/2021/07/15/1626327681155bd693a300469e6614d221906a2859_thumbnail_600x.webp";
+}
+
+function pulseira2Frente() {
+  document.getElementById("pulseira2").src="https://img.ltwebstatic.com/images3_pi/2020/12/21/16085189548c40c1fb3bdaad366a737c7db8ab3263_thumbnail_600x.webp";
+}
+
+function pulseira2Costas() {
+  document.getElementById("pulseira2").src="https://img.ltwebstatic.com/images3_pi/2020/12/21/1608518957826d50c2ae1b6a9f57e8159f73ff69bf_thumbnail_600x.webp";
+}
+
+function pulseira3Frente() {
+  document.getElementById("pulseira3").src="https://img.ltwebstatic.com/images3_pi/2021/03/08/16151946628a91dc729d87c9ffa979dd1e39aeef2f_thumbnail_600x.webp";
+}
+
+function pulseira3Costas() {
+  document.getElementById("pulseira3").src="https://img.ltwebstatic.com/images3_pi/2021/03/08/16151946710c319f61fbecf17c0abf42e67f86f29c_thumbnail_600x.webp";
+}
+//CÓDIGO JS DOS ANÉIS
+function anel1Frente() {
+  document.getElementById("anel1").src="https://img.ltwebstatic.com/images3_pi/2021/09/23/1632391321cb03d98fbdb3309247c08e262115b750_thumbnail_600x.webp";
+}
+
+function anel1Costas() {
+  document.getElementById("anel1").src="https://img.ltwebstatic.com/images3_pi/2021/09/23/16323913231d8634e32bbfa4e165ffb30dd3860021_thumbnail_600x.webp";
+}
+ 
+function anel2Frente() {
+  document.getElementById("anel2").src="https://img.ltwebstatic.com/images3_pi/2021/07/01/16251035560b4bd57188a62eb477f05241da38497a_thumbnail_600x.webp";
+}
+
+function anel2Costas() {
+  document.getElementById("anel2").src="https://img.ltwebstatic.com/images3_pi/2021/07/01/1625103558f0410b89c4e75e3f70a76ed6cb704a66_thumbnail_600x.webp";
+}
+
+function anel3Frente() {
+  document.getElementById("anel3").src="https://img.ltwebstatic.com/images3_pi/2021/08/17/16291902622cbdf7b8d8473ae1c691e40fde19391c_thumbnail_600x.webp";
+}
+
+function anel3Costas() {
+  document.getElementById("anel3").src="https://img.ltwebstatic.com/images3_pi/2021/08/17/162919026495914cd96104cbc35448b6ec70559388_thumbnail_600x.webp";
+}
+//CÓDIGO JS DOS CALÇADOS
+function calcado1Frente() {
+  document.getElementById("calcado1").src="https://img.ltwebstatic.com/images3_pi/2020/08/07/1596773746dc7049ba05552b4815c6c5f4fe3e2c31_thumbnail_600x.webp";
+}
+
+function calcado1Costas() {
+  document.getElementById("calcado1").src="https://img.ltwebstatic.com/images3_pi/2020/08/07/15967737622795acb07d85291a27d438e70393a1a0_thumbnail_600x.webp";
+}
+
+function calcado2Frente() {
+  document.getElementById("calcado2").src="https://img.ltwebstatic.com/images3_pi/2021/06/04/16227853665f5e6e70d5f835e952f092c056588877_thumbnail_600x.webp";
+}
+
+function calcado2Costas() {
+  document.getElementById("calcado2").src="https://img.ltwebstatic.com/images3_pi/2021/06/04/1622785356ffa9f9c8cce3cdf75149505d13a335b4_thumbnail_600x.webp";
+}
+ 
+function calcado3Frente() {
+  document.getElementById("calcado3").src="https://img.ltwebstatic.com/images3_pi/2021/06/02/16226175449ec4a7a3597e3ec02b166826c665db69_thumbnail_600x.webp";
+}
+
+function calcado3Costas() {
+  document.getElementById("calcado3").src="https://img.ltwebstatic.com/images3_pi/2021/06/02/162261754894bba085a4154efaa81bfbb8257a8039_thumbnail_600x.webp";
+}
+
+function calcado4Frente() {
+  document.getElementById("calcado4").src="https://img.ltwebstatic.com/images3_pi/2021/08/02/16278825532252039a2f3fa26c52c7fd037575badd_thumbnail_600x.webp";
+}
+
+function calcado4Costas() {
+  document.getElementById("calcado4").src="https://img.ltwebstatic.com/images3_pi/2021/08/02/1627882551f9d839522eb0e861da2dbdbf7ad6f6c7_thumbnail_600x.webp";
+}
+ 
+function calcado5Frente() {
+  document.getElementById("calcado5").src="https://img.ltwebstatic.com/images3_pi/2021/09/03/16306329294273f63abc4ef391a2ad0011ea5df2d7_thumbnail_600x.webp";
+}
+
+function calcado5Costas() {
+  document.getElementById("calcado5").src="https://img.ltwebstatic.com/images3_pi/2021/09/03/16306329318770e2ba0fb2ea384ed675ca5c5cf80e_thumbnail_600x.webp";
+}
+
+function calcado6Frente() {
+  document.getElementById("calcado6").src="https://img.ltwebstatic.com/images3_pi/2020/11/11/160507751346c42ed8f3a6c0fa5c48477d95ed3353_thumbnail_600x.webp";
+}
+
+function calcado6Costas() {
+  document.getElementById("calcado6").src="https://img.ltwebstatic.com/images3_pi/2020/11/11/16050775110e2b97f0491994ef025c4789ade1af9d_thumbnail_600x.webp";
+}
+
+function calcado7Frente() {
+  document.getElementById("calcado7").src="https://img.ltwebstatic.com/images3_pi/2021/08/11/1628650132e420984d5ecc50935fde5405485c4184_thumbnail_600x.webp";
+}
+
+function calcado7Costas() {
+  document.getElementById("calcado7").src="https://img.ltwebstatic.com/images3_pi/2021/08/11/1628650133c188d1debf711bc936f7fd475c07bdc1_thumbnail_600x.webp";
+}
+
+function calcado8Frente() {
+  document.getElementById("calcado8").src="https://img.ltwebstatic.com/images3_pi/2021/09/11/1631373962a504c02216d184885780ef80b43e19aa_thumbnail_600x.webp";
+}
+
+function calcado8Costas() {
+  document.getElementById("calcado8").src="https://img.ltwebstatic.com/images3_pi/2021/09/11/1631373969bee6516096feb7d2db7250f36b2eb0da_thumbnail_600x.webp";
+}
+ 
+function calcado9Frente() {
+  document.getElementById("calcado9").src="https://img.ltwebstatic.com/images3_pi/2021/08/12/162876834069e608b57dc7fd81ac355a4cfa267a25_thumbnail_600x.webp";
+}
+
+function calcado9Costas() {
+  document.getElementById("calcado9").src="https://img.ltwebstatic.com/images3_pi/2021/08/12/1628768335de5af4572fbbded329b7ecd408de2a3c_thumbnail_600x.webp";
+}
+//CÓDIGO JS DAS BOLSAS 
+function bolsa1Frente() {
+  document.getElementById("bolsa1").src="https://img.ltwebstatic.com/images3_pi/2021/07/07/162564743714f5bbbaf917d805d1ac6ba2e7f95e26_thumbnail_600x.webp";
+}
+
+function bolsa1Costas() {
+  document.getElementById("bolsa1").src="https://img.ltwebstatic.com/images3_pi/2021/07/07/16256474420fa012f77d9b978c3810cca14980e535_thumbnail_600x.webp";
+}
+
+function bolsa2Frente() {
+  document.getElementById("bolsa2").src="https://img.ltwebstatic.com/images3_pi/2021/08/04/162805765613ca6d33c2da0ee7d8dd5844066c72d6_thumbnail_600x.webp";
+}
+
+function bolsa2Costas() {
+  document.getElementById("bolsa2").src="https://img.ltwebstatic.com/images3_pi/2021/08/04/1628057663ad6fe625e910898e5bb27f29f5fa8358_thumbnail_600x.webp";
+}
+
+function bolsa3Frente() {
+  document.getElementById("bolsa3").src="https://img.ltwebstatic.com/images3_pi/2021/09/28/163281913515fdcb27f406e22277fe58d590c1c163_thumbnail_600x.webp";
+}
+
+function bolsa3Costas() {
+  document.getElementById("bolsa3").src="https://img.ltwebstatic.com/images3_pi/2021/09/28/1632819139c9713bfa33ddb062e434802396a6fa09_thumbnail_600x.webp";
+}
+
+function bolsa4Frente() {
+  document.getElementById("bolsa4").src="https://img.ltwebstatic.com/images3_pi/2021/09/15/16317067453e53f55cd34e66aa6fbe86d88e22ba83_thumbnail_600x.webp";
+}
+
+function bolsa4Costas() {
+  document.getElementById("bolsa4").src="https://img.ltwebstatic.com/images3_pi/2021/09/15/16317067418a14341da846919acca76e85fd766d80_thumbnail_600x.webp";
+}
+
+function bolsa5Frente() {
+  document.getElementById("bolsa5").src="https://img.ltwebstatic.com/images3_pi/2021/09/16/1631786918f60cd9ec842c63e5d81e5318e38d7197_thumbnail_600x.webp";
+}
+
+function bolsa5Costas() {
+  document.getElementById("bolsa5").src="https://img.ltwebstatic.com/images3_pi/2021/09/16/1631786921fa50e98f51e9820ad1bbd56eddebe1c9_thumbnail_600x.webp";
+}
+
+function bolsa6Frente() {
+  document.getElementById("bolsa6").src="https://img.ltwebstatic.com/images3_pi/2021/08/31/16303866561473eafc019f43414376b4bee7524cd6_thumbnail_600x.webp";
+}
+
+function bolsa6Costas() {
+  document.getElementById("bolsa6").src="https://img.ltwebstatic.com/images3_pi/2021/08/31/1630386660011e7ef991112d4a51a28784ed5f50eb_thumbnail_600x.webp";
+}
+
+function bolsa7Frente() {
+  document.getElementById("bolsa7").src="https://img.ltwebstatic.com/images3_pi/2021/09/29/1632912443e6f9fcf9593a7c81d83babc5a39d1a1b_thumbnail_600x.webp";
+}
+
+function bolsa7Costas() {
+  document.getElementById("bolsa7").src="https://img.ltwebstatic.com/images3_pi/2021/09/27/1632730464ed2e8fa0a775c2e175c645b26f5548ee_thumbnail_600x.webp";
+}
+
+function bolsa8Frente() {
+  document.getElementById("bolsa8").src="https://img.ltwebstatic.com/images3_pi/2020/09/21/1600663017d8cdfed707822afbfcab9212088e868e_thumbnail_600x.webp";
+}
+
+function bolsa8Costas() {
+  document.getElementById("bolsa8").src="https://img.ltwebstatic.com/images3_pi/2020/09/21/1600663023eb7e8eccd4ea7ae0548c86657b874eb2_thumbnail_600x.webp";
+}
+
+function bolsa9Frente() {
+  document.getElementById("bolsa9").src="https://img.ltwebstatic.com/images3_pi/2021/07/19/1626657812f93af575ab5587b3925f2e14b1b8f4cd_thumbnail_600x.webp";
+}
+
+function bolsa9Costas() {
+  document.getElementById("bolsa9").src="https://img.ltwebstatic.com/images3_pi/2021/07/19/1626657824e661c9aa383bf1f9dafd73c3b4104455_thumbnail_600x.webp";
+}
